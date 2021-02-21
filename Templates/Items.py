a02c3bd56b78e6c918dcbc3768efaa144af617f0
Fabian Dill <fabian.dill@web.de>
2021-02-21 14:54:08 +0100

    add Item id table

--- Templates/Items.py
+++ Templates/Items.py
@@ -1,4 +1,5 @@
 items = \
 {{items|safe}}
 
-item_table = {data["name"] for item_id, data in items.items()}
+item_table = {data["name"]: item_id for item_id, data in items.items()}
+lookup_id_to_name = {item_id: data["name"] for item_id, data in items.items()}
